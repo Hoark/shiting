f5b3e0962a158f9cdeb617f0cd0400dff4e6ed6b
Daniel Fang <76408862+Hoark@users.noreply.github.com>
2021-12-12 17:24:20 +0800

    split in segments of 500ms

--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
 import json
-import torch, torchaudio
+import torch, torchaudio, torchvision
 from torch.utils.data import DataLoader, Dataset, random_split
 import random
 import os
@@ -8,10 +8,11 @@ import numpy as np
 import torch.nn.functional as F
 from torch.nn import init
 import torch.nn as nn
+from statistics import mode
 
 # Globals
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-g_sample_ms = 4000 # 3 sec
+g_sample_ms = 500 # 500ms
 g_sample_len = utils.g_sr // 1000 * g_sample_ms  # Floor division
 
 #Audio Processing Utilities
@@ -94,11 +95,11 @@ class AudioUtil:
         return (sig.roll(shift_amt), sr)
 
     @staticmethod
-    def spectro_gram(aud, n_mels=64, n_fft=1024, hop_len=None):
+    def spectro_gram(aud, n_mfcc=20, n_fft=1024, hop_len=None):
         # Generate a Spectrogram. Spectrograms are better for NN than audio data.
         sig, sr = aud
         top_db = 80
-        spec = torchaudio.transforms.MFCC(sr)(sig) # Use MFCC, better suited for human speech
+        spec = torchaudio.transforms.MFCC(sr, n_mfcc=n_mfcc)(sig) # Use MFCC, better suited for human speech
         return (spec)
 
     @staticmethod
@@ -129,26 +130,23 @@ def index_to_label(index):
 
 class myTrainDataset(Dataset):
   def __init__(self, path):
-    self.labels, self.data_path = self.Data_Load(path)
+    self.labels, self.audio_samples = self.Data_Load(path)
     self.sr = utils.g_sr  # Global Sample Rate
     self.seg_dur = g_sample_ms # 500 ms
-    self.shift_pct = 0.3  # Time shift. We do not use it (for now)
-
+    self.shift_pct = g_sample_ms*0.1  # Time shift. We do not use it (for now)
 
   def Data_Load(self, path):
     # Load data. Data will be augmented in getitem, so no need to do that here.
-    labels = list(); data_path = list(); sample_size = 200
+    labels = list();
+    audio_samples = list()
     for label in os.listdir(f'{path}'):
-      nfiles = len(os.listdir(f'{path}/{label}'))
-      ncopies = -(sample_size//-nfiles) # Ceiling Division
       for filename in os.listdir(f'{path}/{label}'):
         filepath = f'./{path}/{label}/{filename}'
-        aud = np.transpose(utils.read_audio(filepath))  # Transpose to match AudioUtils
-        aud = torch.tensor(aud)
-        for i in range(ncopies):
+        audio = utils.read_audio(filepath)
+        for i in range(audio.shape[0] // g_sample_len):
           labels.append(label)
-          data_path.append(aud)
-    return labels, data_path
+          audio_samples.append(audio[i * g_sample_len:(i + 1) * g_sample_len, :])
+    return labels, audio_samples
 
   def __len__(self):
     return len(self.labels)
@@ -158,16 +156,17 @@ class myTrainDataset(Dataset):
     target = label_to_index(self.labels[idx])
 
     # Load audio file
-    aud = self.data_path[idx]
-    aud_segment = AudioUtil.pad_trunc([aud, utils.g_sr], self.seg_dur)
+    aud =  self.audio_samples[idx]
+    aud = torch.transpose(torch.tensor(aud),0,1)
+    #aud_segment = AudioUtil.pad_trunc([aud, utils.g_sr], self.seg_dur)
     # Generalise data
     #dur_aud = AudioUtil.pad_trunc([aud, utils.g_sr], self.duration) # Extract a random 4 sec segment out of the entire audio file
 
     # Augmentations
-    shift_aud = AudioUtil.time_shift(aud_segment, self.shift_pct) # Not Needed. Speech has time-correlation a time shift could mess that up
-    sgram = AudioUtil.spectro_gram(shift_aud, n_mels=64, n_fft=1024, hop_len=None) # Generate a spectrogram
-    rnd_freq = random.randint(0,3+1) #randint(a,b+1) picks random int from a<=N<=b
-    rnd_time = random.randint(0,3+1)
+    #shift_aud = AudioUtil.time_shift(aud_segment, self.shift_pct) # Not Needed. Speech has time-correlation a time shift could mess that up
+    sgram = AudioUtil.spectro_gram([aud, utils.g_sr]) # Generate a spectrogram
+    rnd_freq = random.randint(0,2+1) #randint(a,b+1) picks random int from a<=N<=b
+    rnd_time = random.randint(0,2+1)
     aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=rnd_freq, n_time_masks=rnd_time) # Not Needed (for now)
 
     end_sgram = aug_sgram
@@ -186,9 +185,7 @@ class myTestDataset(Dataset):
         for file_name in os.listdir(path):
           labels.append(task2_gt[file_name])
           filepath = f'./{path}/{file_name}'
-          aud = np.transpose(utils.read_audio(filepath))  # Transpose to match AudioUtils
-          aud = torch.tensor(aud)
-          data_path.append(aud)
+          data_path.append(filepath)
       return labels, data_path
 
     def __len__(self):
@@ -199,12 +196,18 @@ class myTestDataset(Dataset):
       target = label_to_index(self.labels[idx])
 
       # Load audio file
-      aud = self.data_path[idx]
-      aud_segment = AudioUtil.pad_trunc([aud, utils.g_sr], self.seg_dur)
-      sgram = AudioUtil.spectro_gram(aud_segment, n_mels=64, n_fft=1024, hop_len=None)  # Generate a spectrogram
+      audio_path = self.data_path[idx]
+      audio = utils.read_audio(audio_path)
+      audio = torch.transpose(torch.tensor(audio),0,1)
+      sgrams = list()
+      for i in range(audio.shape[1] // g_sample_len):
+        aud_segment = (audio[:,i * g_sample_len:(i + 1) * g_sample_len])
+        sgrams.append(AudioUtil.spectro_gram([aud_segment, utils.g_sr]))
+      #aud_segment = AudioUtil.pad_trunc([aud, utils.g_sr], self.seg_dur)
+      #sgram = AudioUtil.spectro_gram(aud)  # Generate a spectrogram
       # aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2) # Not Needed (for now)
 
-      end_sgram = sgram
+      end_sgram = sgrams
       return end_sgram, target
 
 # Audio Classification Model
@@ -214,43 +217,22 @@ class AudioClassifier(nn.Module):
     def __init__(self):
         super().__init__()
         conv_layers = []
+        self.layer = [4,8,16,32,64,64,128,128,128]
 
-        # 1st Convolution Block with Relu and Batch Norm. Use Kaiming Initialization
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2))
-        self.relu1 = nn.ReLU()
-        self.bn1 = nn.BatchNorm2d(4)
-        init.kaiming_normal_(self.conv1.weight, a=0.1)
-        self.conv1.bias.data.zero_()
-        conv_layers += [self.conv1, self.relu1, self.bn1]
-
-        # 2nd Convolution Block
-        self.conv2 = nn.Conv2d(4, 8, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
-        self.relu2 = nn.ReLU()
-        self.bn2 = nn.BatchNorm2d(8)
-        init.kaiming_normal_(self.conv2.weight, a=0.1)
-        self.conv2.bias.data.zero_()
-        conv_layers += [self.conv2, self.relu2, self.bn2]
-
-        # 3rd Convolution Block
-        self.conv3 = nn.Conv2d(8, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
-        self.relu3 = nn.ReLU()
-        self.bn3 = nn.BatchNorm2d(16)
-        init.kaiming_normal_(self.conv3.weight, a=0.1)
-        self.conv3.bias.data.zero_()
-        conv_layers += [self.conv3, self.relu3, self.bn3]
-
-        # 4th Convolution Block
-        self.conv4 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
-        self.relu4 = nn.ReLU()
-        self.bn4 = nn.BatchNorm2d(32)
-        init.kaiming_normal_(self.conv4.weight, a=0.1)
-        self.conv4.bias.data.zero_()
-        conv_layers += [self.conv4, self.relu4, self.bn4]
+        in_channels = 1
+        for v in self.layer:
+          conv2d = nn.Conv2d(in_channels=in_channels, out_channels=v, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+          bn2d = nn.BatchNorm2d(v)
+          relu2d = nn.ReLU()
+          init.kaiming_normal_(conv2d.weight, a=0.1)
+          conv2d.bias.data.zero_()
+          conv_layers += [conv2d, bn2d, relu2d]
+          in_channels = v
 
         # Linear Classifier
         self.ap = nn.AdaptiveAvgPool2d(output_size=1)
         # Change in_features when changing conv. layers
-        self.lin = nn.Linear(in_features=32, out_features=20) #Input is an array of size 64, and we have 20 possible outputs (The bird species)
+        self.lin = nn.Linear(in_features=self.layer[-1], out_features=20) #Input is an array of size 64, and we have 20 possible outputs (The bird species)
 
         # Wrap the Convolutional Blocks
         self.conv = nn.Sequential(*conv_layers)
@@ -321,21 +303,28 @@ def testing(model, val_dl):
   running_loss = 0; criterion = nn.CrossEntropyLoss()
   # Disable gradient updates
   with torch.no_grad():
-    for data in val_dl:
+    for i, data in enumerate(val_dl):
+      predictions = list()
       # Get the input features and target labels, and put them on the GPU
-      inputs, labels = data[0].to(device), data[1].to(device)
-      # Normalize the inputs
-      inputs_m, inputs_s = inputs.mean(), inputs.std()
-      inputs = (inputs - inputs_m) / inputs_s
-      # Get predictions
-      outputs = model(inputs)
-      loss = criterion(outputs, labels)
-      running_loss += loss.item()
-      # Get the predicted class with the highest score
-      _, prediction = torch.max(outputs, 1)
+      inlist, label = data[0], data[1].to(device)
+      for inputs in inlist:
+        inputs = inputs.to(device)
+        # Normalize the inputs
+        inputs_m, inputs_s = inputs.mean(), inputs.std()
+        inputs = (inputs - inputs_m) / inputs_s
+
+        # Get predictions
+        outputs = model(inputs)
+        loss = criterion(outputs, label)
+        running_loss += loss.item()
+        # Get the predicted class with the highest score
+        _, predict= torch.max(outputs, 1)
+        predict = predict.item()
+        predictions.append(predict)
+      prediction = mode(predictions)
       # Count of predictions that matched the target label
-      correct_prediction += (prediction == labels).sum().item()
-      total_prediction += prediction.shape[0]
+      correct_prediction += (prediction == label.item())
+      total_prediction += 1
 
   acc = correct_prediction / total_prediction
   num_batches = len(val_dl)
@@ -344,17 +333,32 @@ def testing(model, val_dl):
 
 # Main
 def run(train_path, test_path):
+  print('Start DataSet and DataLoader')
   # Dataset
   train_ds = myTrainDataset(train_path)
   test_ds = myTestDataset(test_path)
   # DataLoader
-  train_dl = torch.utils.data.DataLoader(train_ds, batch_size=32, shuffle=True)
-  test_dl = torch.utils.data.DataLoader(test_ds, batch_size=32, shuffle=False)
+  train_dl = torch.utils.data.DataLoader(train_ds, batch_size=16, shuffle=True)
+  test_dl = torch.utils.data.DataLoader(test_ds, batch_size=1, shuffle=False)
+  print('Finish DataSet and DataLoader')
 
+  '''
   myModel = AudioClassifier()
   myModel = myModel.to(device)
+  '''
+  model = torchvision.models.resnet18(pretrained=True)
+  model.conv1 = nn.Conv2d(1, model.conv1.out_channels,
+                          kernel_size=model.conv1.kernel_size[0],
+                          stride=model.conv1.stride[0],
+                          padding=model.conv1.padding[0])
+  num_ftrs = model.fc.in_features
+  model.fc = nn.Linear(num_ftrs, 20)
+  myModel = model
+  myModel = myModel.to(device)
 
-  fail_cnt = 0; fail_tresh = 5; fail_loss = 99
+  #myModel = torch.hub.load('pytorch/vision:v0.10.0', 'vgg11', pretrained=True)
+
+  fail_cnt = 0; fail_tresh = 10; fail_loss = 99
   nEpoch = 0
   print('Started Training and Testing')
   while True:
